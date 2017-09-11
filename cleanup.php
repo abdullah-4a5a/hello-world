eda544520024d04a07874443c2bb1f5e41ddc083
abdullah-4a5a <501419@edu.rocmn.nl>
2017-09-11 14:25:08 +0200

    Add files via upload

---
+++ cleanup.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<body>
+
+<?php
+
+  $text = "  dEzE TeKSt wOrDt opgeSchoond ";
+  $result = trim($text);
+  $result = ucwords(strtolower($text));
+  echo $result;
+
+?>
+
+</body>
+</html>
