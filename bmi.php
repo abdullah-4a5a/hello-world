1ee8e167f6aad4d153ac252959cb62b9ca0dbaea
abdullah-4a5a <501419@edu.rocmn.nl>
2017-11-10 16:43:22 +0100

    Update bmi.php

--- bmi.php
+++ bmi.php
@@ -1,28 +1,70 @@
-<doctype! html>
-<head>
-</head>
-<body>
-
 <?php
-
-$result = function(){
-$gewicht = $_POST['gewicht'];
-$lengte = $_POST['lengte'];
-
-$lengteSum = $lengte * $lengte;
-$sum = $gewicht / $lengteSum;
-echo number_format($sum,2);
+session_start();
+?>
+<?php
+if(!ISSET($_SESSION['colour'])){
+  $_SESSION['colour'] = $_POST['colour'];
+}
+else{
+  $_SESSION['colour'];
 }
 ?>
-  <form method="post">
-    Gewicht<br>
-    <input type="text" name="gewicht">
-    <br>
-    Lengte<br>
-    <input type="text" name="lengte">
-    <br><br>
-    <input type="submit" value="Bereken">
-    <p id="result"><?php echo $result() ?></p>
-  </form>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form method='POST' action=''>
+      <input onchange='setColour()' name='colour' type='color' id='colour' />
+      Gewicht: <input type='number' name='gewicht' />
+      Lengte: <input type='number' step='.01' name='lengte' />
+      <input type='submit' name='submit' value='Berekenen' />
+    </form>
+    <script>
+      document.body.style.backgroundColor = ' <?= $_SESSION['colour'] ?> ';
+    </script>
+
+<script>
+var colour;
+  function setColour(){
+    var colour = document.querySelector('#colour').value;
+  }
+</script>
+<?php
+$lengte;
+$gewicht;
+$bmi = array();
+$bmi2 = array();
+$_SESSION['uno'] = $bmi;
+$_SESSION['dos'] = $bmi2;
+if(ISSET($_POST['submit'])){
+  $lengte = $_POST['lengte'];
+  $gewicht = $_POST['gewicht'];
+  $colour = $_POST['colour'];
+  $_SESSION['colour'] = $colour;
+  echo 'BMI is: ' . calculate($lengte, $gewicht);
+  ?>
+  <script>
+    document.body.style.backgroundColor = ' <?= $_SESSION['colour'] ?> ';
+  </script>
+  <?php
+  array_push($bmi, $gewicht, $lengte, calculate($lengte, $gewicht));
+  array_push($bmi2, $bmi);
+}
+function calculate($lengte, $gewicht){
+  $bmiCalc = $gewicht/($lengte*$lengte);
+  $result = round($bmiCalc, 2);
+  return $result;
+
+}
+echo "<table border='1'><tr><th>Gewicht</th><th>Lengte</th><th>bmi</th></tr>";
+foreach ($bmi2 as $value) {
+    echo "<tr><td>$value[0]</td><td>$value[1]</td><td>$value[2]</td></tr>";
+}
+echo "</table>";
+?>
 </body>
 </html>
